df07c98ef6c2689dfc57458c9ddf963a6a59ec4f
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-02-27 01:01:39 +0100

    Updates

---
+++ precache-manifest.1039cd45ceab77229c54e52de3483f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1622778ce1456d29d70f03f3c6276826",
+    "url": "/pogodynka/index.html"
+  },
+  {
+    "revision": "ba217cc396c192080a52",
+    "url": "/pogodynka/static/css/main.a3728f82.chunk.css"
+  },
+  {
+    "revision": "3915188f700306814b16",
+    "url": "/pogodynka/static/js/2.836dcabd.chunk.js"
+  },
+  {
+    "revision": "9c7e6cca0df3be74c612f63119a9d27d",
+    "url": "/pogodynka/static/js/2.836dcabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba217cc396c192080a52",
+    "url": "/pogodynka/static/js/main.f739c324.chunk.js"
+  },
+  {
+    "revision": "bebbc9c18714ac19a4f3",
+    "url": "/pogodynka/static/js/runtime-main.10e786b6.js"
+  }
+]);
