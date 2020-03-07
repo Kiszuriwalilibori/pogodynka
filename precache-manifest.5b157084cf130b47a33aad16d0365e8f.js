072ae4a664a6841be8744107d5105c0ddbfdb0c4
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-03-07 11:25:51 +0100

    Updates

---
+++ precache-manifest.5b157084cf130b47a33aad16d0365e8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d011c23881e6abb92514d0c662e74d",
+    "url": "/pogodynka/index.html"
+  },
+  {
+    "revision": "8a0e574dd068d6540a56",
+    "url": "/pogodynka/static/css/main.cdf14ee4.chunk.css"
+  },
+  {
+    "revision": "b23b7fd5d98f365453d8",
+    "url": "/pogodynka/static/js/2.f543f640.chunk.js"
+  },
+  {
+    "revision": "9c7e6cca0df3be74c612f63119a9d27d",
+    "url": "/pogodynka/static/js/2.f543f640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0e574dd068d6540a56",
+    "url": "/pogodynka/static/js/main.110614e0.chunk.js"
+  },
+  {
+    "revision": "bebbc9c18714ac19a4f3",
+    "url": "/pogodynka/static/js/runtime-main.10e786b6.js"
+  },
+  {
+    "revision": "a0c8e79d73a16435caf1466d138b54ec",
+    "url": "/pogodynka/static/media/back1.a0c8e79d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbe9a12eeefd0aba7bb7a5e6018dc37b.js"
+  "/pogodynka/precache-manifest.5b157084cf130b47a33aad16d0365e8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pogodynka/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
