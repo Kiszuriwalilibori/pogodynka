fb175f6bfb72ce45301340735bbe5228babdf54e
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2024-02-04 02:42:41 +0100

    useDelayed now with minimal action time

--- src/hooks/useDelayedCondition.ts
+++ src/hooks/useDelayedCondition.ts
@@ -1,20 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 
 const DELAY = 1000;
+const DURATION = 1000;
 
-export const useDelayedCondition = (condition: boolean, delay = DELAY) => {
+export const useDelayedCondition = (condition: boolean, delay = DELAY, duration = DURATION) => {
   const [delayedCondition, setDelayedCondition] = useState(false);
   const delayTimeout = useRef(undefined as undefined | NodeJS.Timeout);
+  const startTime = useRef(undefined as undefined | number);
+  const durationTimeout = useRef(undefined as undefined | NodeJS.Timeout);
 
   useEffect(() => {
     if (condition) {
+      startTime.current = Date.now();
       delayTimeout.current = setTimeout(() => {
         setDelayedCondition(true);
       }, delay);
     }
     if (!condition) {
-      delayTimeout.current && clearTimeout(delayTimeout.current);
-      setDelayedCondition(false);
+      if (startTime.current) {
+        if (Date.now() >= startTime.current + delay) {
+          durationTimeout.current = setTimeout(() => {
+            delayTimeout.current && clearTimeout(delayTimeout.current);
+            setDelayedCondition(false);
+          }, duration - (Date.now() - (startTime.current + DELAY)));
+        }
+        if (Date.now() < startTime.current + delay) {
+          delayTimeout.current && clearTimeout(delayTimeout.current);
+          setDelayedCondition(false);
+        }
+      }
     }
   }, [condition]);
 
