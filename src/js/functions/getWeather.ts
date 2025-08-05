69b8eeb6bdbe94b5f21e2401a75621f4b93c5569
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-08-05 10:07:43 +0200

    fixed error catching in getWeather

--- src/js/functions/getWeather.ts
+++ src/js/functions/getWeather.ts
@@ -1,7 +1,64 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { showErrorMessage } from "../Redux/actionCreators";
 
-export default async function getWeather(url: string) {
-  const { data } = await axios.get(url);
+interface WeatherData {
+  
+  temp: number;          
+  feels_like: number;    
+  temp_min: number;      
+  temp_max: number;     
+  
+  humidity: number;      
+  pressure: number;      
+  
+  description: string;   
+  icon: string;          
+  
+  windSpeed: number;     
+  windDirection: number; 
+  
+  dt: number;            
+  dt_txt: string;        
+}
 
-  return data as Object | undefined;
+interface WeatherError {
+  message?: string;
+  error?: string;
+  code?: number;
+}
+
+export default async function getWeather(url: string): Promise<WeatherData | undefined> {
+  try {
+    const response = await axios.get<WeatherData>(url);
+    
+    if (!response.data) {
+      throw new Error('No data received from weather API');
+    }
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError<WeatherError>;
+      
+      if (axiosError.response) {
+        // Server responded with error status
+        const errorMessage = axiosError.response.data?.message || 
+                           axiosError.response.data?.error || 
+                           axiosError.response.statusText;
+        showErrorMessage(`Server error: ${errorMessage}`);
+      } else if (axiosError.request) {
+        // Request made but no response
+        showErrorMessage('No response from weather API');
+      } else {
+        // Error in setting up the request
+        showErrorMessage('Error creating request to weather API');
+      }
+    } else {
+      // Non-axios error
+      showErrorMessage('Error fetching weather data');
+    }
+
+    console.error('Weather API error:', error);
+    return undefined;
+  }
 }
