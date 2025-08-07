d74790a7b0be2ba078f238975a793a0e5e95e367
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-08-07 09:06:03 +0200

    reverted SearchInFavorites.styles changes

--- src/Pages/SearchPage/parts/SearchInFavorites/SearchInFavorites.styles.tsx
+++ src/Pages/SearchPage/parts/SearchInFavorites/SearchInFavorites.styles.tsx
@@ -1,3 +1,82 @@
+// import makeStyles from "@mui/styles/makeStyles";
+// import theme from "themes/theme";
+
+// export const classes ={
+//   formControl: {
+//     margin: theme.spacing(1),
+//     minWidth: 250,
+//     "& .MuiOutlinedInput-notchedOutline": {
+//       border: "none !important",
+//     },
+//   },
+
+//   inputLabel: {
+//     color: "black !important",
+//     paddingLeft: 4,
+//     fontWeight: 700,
+//     letterSpacing: "0.00938em",
+//   },
+//   dropdownStyle: {
+//     border: "4px solid orange",
+//     backgroundColor: "lightgrey",
+//     marginTop: -7,
+//     color: "black",
+//   },
+//   fieldset: { border: "none !important" },
+//   select: {
+//     backgroundColor: "transparent",
+//     "&:before": {
+//       // normal
+//       borderBottom: "none",
+//       backgroundColor: "transparent",
+//     },
+//     "&:after": {
+//       // focused
+//       borderBottom: "none",
+//       backgroundColor: "transparent",
+//     },
+//     "&:hover:not(.Mui-disabled):not(.Mui-focused):not(.Mui-error):before": {
+//       // hover
+//       borderBottom: "none",
+//       backgroundColor: "transparent",
+//     },
+//   },
+
+//   selectEmpty: {
+//     marginTop: theme.spacing(2),
+//   },
+//   icon: {
+//     fill: "green",
+//   },
+// };
+
+// export const paperSx = { padding: "0", marginTop: "60px" };
+
+// export const inputLabelSx = {
+//   color: "black !important",
+//   paddingLeft: 4,
+//   fontWeight: 700,
+//   letterSpacing: "0.00938em",
+// };
+
+// export const selectSx = {
+//   backgroundColor: "transparent",
+//   "&:before": {
+//     // normal
+//     borderBottom: "none",
+//     backgroundColor: "transparent",
+//   },
+//   "&:after": {
+//     // focused
+//     borderBottom: "none",
+//     backgroundColor: "transparent",
+//   },
+//   "&:hover:not(.Mui-disabled):not(.Mui-focused):not(.Mui-error):before": {
+//     // hover
+//     borderBottom: "none",
+//     backgroundColor: "transparent",
+//   },
+// };
 import makeStyles from "@mui/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
